03cb744a7df2388ae2315989837a63b795d8751f
Ida <ida.bergstrom@medieinstitutet.se>
2018-08-15 13:00:38 +0200

    From database to DOM, js calendar, SASS

--- index.php
+++ index.php
@@ -8,21 +8,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <style>
     body {
         background-color: lightgray;
+      font-family: sans-serif;
+      font-weight: 300;
     }
     .output{
         background-color: white;
-        width: 200px;
-        height: 200px;
+        width: 100px;
+        height: 100px;
     }
+}
+
+
     
 </style>
 
+
+<body>
+    <div class="output"></div>
+    
+    <div id="calendar"></div>
+    
+
 <script>
+    
+
+    
+    
+    /* Database connection & output */
+    
+    const outputDiv = document.querySelector('.output');
+    
     function reqListener () {
       console.log(this.responseText);
     }
@@ -33,15 +54,24 @@
         //The actual data is found on this.responseText
         //alert(this.responseText); //Will alert: 42
         console.log(this.responseText);
+        
+        let dataParsed = JSON.parse(this.responseText);
+        console.log(dataParsed[0].name);
+        let output = dataParsed[0].name;
+        outputDiv.innerText = output;
     };
     oReq.open("get", "get-data.php", true);
     //                               ^ Don't block the rest of the execution.
     //                                 Don't wait until the request finishes to 
     //                                 continue.
     oReq.send();
+    
+
 </script>
 
-<body>
-    <div class="output"></div>
+<script src="calendar.js"></script>
+
+
+
 </body>
 </html>
